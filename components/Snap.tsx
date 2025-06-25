d60d3c33a87ea18888f7c4396980c6d8b45ade5d
DevGhost511 <rosemax994@gmail.com>
2025-06-25 18:25:28 +0900

    first commit

---
+++ components/Snap.tsx
@@ -0,0 +1,13 @@
+export default function Snap() {
+    return (
+        <div className="flex flex-row items-center justify-around gap-3 border-1 border-(--brand-6) h-12 w-full fill-white px-4">
+            <div className="flex flex-row items-center justify-start gap-4 w-full">
+                <img src="plus.svg" alt="icon" />
+                <input type="text" placeholder="Ask Olle about your collectible or just ask anything" className="outline-none font-Geist text-(--black-5) placeholder:text-(--black-4) font-medium text-xl text-left w-full border-0 " />
+
+            </div>
+            <img src="camera.svg" alt="icon" />
+
+        </div>
+    )
+}
