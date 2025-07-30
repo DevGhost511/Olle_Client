83a96256926570a0ba7452fcc200688a8f867d9f
DevGhost511 <rosemax994@gmail.com>
2025-07-31 03:13:10 +0900

    feat: upload url

--- src/app/snap/page.tsx
+++ src/app/snap/page.tsx
@@ -127,7 +127,8 @@ export default function Page() {
       const imageUrl = localStorage.getItem('imageUrl');
       if (imageUrl) {
         setImage(process.env.NEXT_PUBLIC_API_URL + '/images/' + imageUrl);
-        imageIdentify("thread_3v23Z3JEG0eom2bjrea0aeVw", "I just want to identify a collection in this image. Car, Watch or Art collection.  give me name(detailed model including), Rarerate, Price(10 numbers for every 6 monthes from 2020 to now ), one-paragraph Description(including produce date and where it is produced) about that. Rarerate should be one value of 1,2,3,4,5.  If this image doesn't include any collection answer there isn't any collection. No need any complex context. Only give me 3 words in this style so that I can parse to json it: { \"name\" : String, \"price\": number[], \"rarerate\": number, \"description\" : String } ", "https://beige-managerial-gull-792.mypinata.cloud/ipfs/bafybeifw2do4c2gfbrzdspxeepmuu3wolcbikcj2jaflfyt6swxbvnebui")
+        console.log(image);
+        imageIdentify("thread_3v23Z3JEG0eom2bjrea0aeVw", "I just want to identify a collection in this image. Car, Watch or Art collection.  give me name(detailed model including), Rarerate, Price(10 numbers for every 6 monthes from 2020 to now ), one-paragraph Description(including produce date and where it is produced) about that. Rarerate should be one value of 1,2,3,4,5.  If this image doesn't include any collection answer there isn't any collection. No need any complex context. Only give me 3 words in this style so that I can parse to json it: { \"name\" : String, \"price\": number[], \"rarerate\": number, \"description\" : String } ", imageUrl)
           .then(res => {
             console.log(res)
             const obj = JSON.parse(res.reply)
