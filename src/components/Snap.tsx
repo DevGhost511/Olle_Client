aa9dd70e15e94fe59a036f3afd6a95631876a9ac
monsterdev914 <skytech2510@gmail.com>
2025-08-01 21:33:14 -0500

    chore: clean code

--- src/components/Snap.tsx
+++ src/components/Snap.tsx
@@ -7,10 +7,11 @@ import { compressImage } from "@/utils";
 type Props = {
     onChange: (prompt: string) => void;
     onImageUpload?: () => void;
+    isDisabled?: boolean;
 }
 
-export default function Snap({ onChange, onImageUpload }: Props) {
-
+export default function Snap({ onChange, onImageUpload, isDisabled }: Props) {
+    const router = useRouter();
     const fileInputRef = useRef<HTMLInputElement>(null);
     const [isUploading, setIsUploading] = useState(false);
 
@@ -59,8 +60,8 @@ export default function Snap({ onChange, onImageUpload }: Props) {
         }
     }
 
-    const router = useRouter();
     const handleNavigation = () => {
+        //reload the page
         router.push('/snap');
     };
 
@@ -68,11 +69,11 @@ export default function Snap({ onChange, onImageUpload }: Props) {
     return (
         <div className="flex flex-col items-center justify-around gap-3 w-full">
 
-            <div className="flex flex-row items-center justify-around gap-3 rounded-xl border border-(--black-2) [&:has(input:focus)]:border-(--brand-6) [&:has(input:focus)]:outline-4 [&:has(input:focus)]:outline-(--brand-3) w-full bg-white px-1 py-1 ">
+            <div className={`flex flex-row items-center justify-center gap-3 rounded-xl border border-(--black-2) [&:has(input:focus)]:border-(--brand-6) [&:has(input:focus)]:outline-4 [&:has(input:focus)]:outline-(--brand-3) w-full px-1 py-1 ${isDisabled ? 'bg-gray-200' : 'bg-white '}`}>
                 <div className="flex flex-row items-center justify-start gap-4 w-full">
                     <button
                         onClick={handlePlusClick}
-                        disabled={isUploading}
+                        disabled={isUploading || isDisabled}
                         className="flex rounded-lg px-2 py-2 cursor-pointer hover:bg-(--brand-2) disabled:opacity-50 disabled:cursor-not-allowed"
                     >
                         {isUploading ? (
@@ -95,8 +96,10 @@ export default function Snap({ onChange, onImageUpload }: Props) {
                             onChange(e.currentTarget.value);
                             e.currentTarget.value = '';
                         }
-                    }} placeholder="Ask Olle about your collectible or just ask anything" className="outline-none font-Geist text-(--black-5) placeholder:text-(--black-4) font-medium text-lg text-left w-full border-0 " />
-                    <button className="flex rounded-lg px-2 py-2 cursor-pointer hover:bg-(--brand-2)">
+                    }}
+                        disabled={isDisabled}
+                        placeholder="Ask Olle about your collectible or just ask anything" className="outline-none font-Geist text-(--black-5) placeholder:text-(--black-4) font-medium text-lg text-left w-full border-0 " />
+                    <button disabled={isDisabled} className="flex rounded-lg px-2 py-2 cursor-pointer hover:bg-(--brand-2)">
                         <img className="w-7 h-7" src="camera.svg" alt="icon" />
                     </button>
                 </div>
